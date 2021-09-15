bfd3c83dbdfaca355168ff294595bb3cff5e55f2
Alan Montgomery <alan.montgomery@tascomi.com>
2021-09-15 19:24:12 +0100

    Added Web QR codes as well

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { defineCustomElements } from '@ionic/pwa-elements/loader';
-import { StatusBar, Style } from '@capacitor/status-bar';
 
 
 ReactDOM.render(
@@ -11,8 +10,6 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// StatusBar.setStyle({ style: Style.Dark });
 
 // Call the element loader after the app has been rendered the first time
 defineCustomElements(window);
